ec7278a1f2dd7e52e07e7c08ec039ffb08f2e3da
Jonny Elliott, Work Machine <jonnynelliott@googlemail.com>
2011-12-21 16:16:16 +0100

    to use as a library

---
+++ __init__.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+"""Default python script layout."""
+
+# import
+
+__author__ = "Jonny Elliott"
+__copyright__ = "Copyright 2011"
+__credits__ =  ""
+__license__ = "GPL"
+__version__ = "0.0"
+__maintainer__ = "Jonny Elliott"
+__email__ = "jonnyelliott@mpe.mpg.de"
+__status__ = "Prototype"
+
+def main():
+	print __doc__
+
+if __name__ == "__main__":
+	main()
+# Mon Dec 19 18:48:02 CET 2011
